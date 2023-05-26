13d9d7c243ada77cc9716bc75a8f8a104cd571f0
davirany <davirany@gmail.com>
2023-05-26 20:23:10 -0300

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/icons8-image-96.png'
+
+export default function Header() {
+    let navigate = useNavigate();
+    return (
+        <header>
+            <nav>
+                <div className="logo-container" onClick={() => navigate('/')}>
+                    <img src={logo} alt="site-logo" />
+                </div>
+                <div className="nav-links">
+                    <div onClick={() => navigate('/')}>Pagina Inicial</div>
+                    <div onClick={() => navigate('/about-us')}>Sobre Nós</div>
+                    <div onClick={() => navigate('/about-the-project')}>Sobre o Projeto</div>
+                </div>
+            </nav>
+        </header>
+    );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+// import './styles/Main.css';
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
