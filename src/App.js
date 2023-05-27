465e188a4b65a6623a9a9cf8831f459eaa2ff746
pehenobra2 <peheguibra@gmail.com>
2023-05-27 16:10:05 -0300

    Implementando a compactação da foto e a implementação do huffman

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Main } from './components/Main';
+import Huffman from "./components/Huffman";
 
-
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Main />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+        <Huffman/>
+      </div>
+    );
+  }
 }
 
 export default App;
