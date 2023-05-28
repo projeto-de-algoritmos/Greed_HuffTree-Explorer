fda07868cab8b98b98e7060f0fce5378343d1881
davirany <davirany@gmail.com>
2023-05-28 03:40:16 -0300

    adicionando o botao para download da imagem

--- src/components/Huffman.js
+++ src/components/Huffman.js
@@ -11,6 +11,16 @@ class Huffman extends React.Component {
     };
   }
 
+  downloadImage = () => {
+    const { compressedImage } = this.state;
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = `data:image/jpeg;base64,${compressedImage}`;
+    downloadLink.download = 'compressed_image.jpg';
+
+    downloadLink.click();
+  };
+
   handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -27,21 +37,16 @@ class Huffman extends React.Component {
   compressImage = () => {
     const { originalImage } = this.state;
 
-    // Crie um elemento canvas para desenhar a imagem
+
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = originalImage.width;
     canvas.height = originalImage.height;
     ctx.drawImage(originalImage, 0, 0);
-
-    // Obtenha os dados dos pixels da imagem
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const pixels = imageData.data;
-
-    // Crie um objeto para armazenar as frequências dos valores dos pixels
     const frequencies = {};
 
-    // Calcule as frequências dos valores dos pixels
     for (let i = 0; i < pixels.length; i += 4) {
       const red = pixels[i];
       const green = pixels[i + 1];
@@ -54,10 +59,7 @@ class Huffman extends React.Component {
       }
     }
 
-    // Construa a árvore de Huffman a partir das frequências dos pixels
     const huffmanTree = buildHuffmanTree(frequencies);
-
-    // Codifique os pixels da imagem usando a árvore de Huffman
     let compressedPixels = '';
     for (let i = 0; i < pixels.length; i += 4) {
       const red = pixels[i];
@@ -68,7 +70,6 @@ class Huffman extends React.Component {
       compressedPixels += huffmanCode;
     }
 
-    // Atualize o estado do componente com a imagem compactada e a árvore de Huffman
     this.setState({
       compressedImage: compressedPixels,
       huffmanTree: huffmanTree,
@@ -99,6 +100,7 @@ class Huffman extends React.Component {
         {compressedImage && (
           <div>
             <h2>Imagem Compactada</h2>
+            <button onClick={this.downloadImage}>Download Imagem Compactada</button>
             <img src={compressedImage.src} alt="Imagem Compactada" />
           </div>
         )}
@@ -110,7 +112,6 @@ class Huffman extends React.Component {
 export default Huffman;
 
 const buildHuffmanTree = (frequencies) => {
-  // Crie nós iniciais com base nas frequências dos pixels
   const nodes = Object.entries(frequencies).map(([pixelValue, frequency]) => ({
     pixelValue,
     frequency,
@@ -118,12 +119,9 @@ const buildHuffmanTree = (frequencies) => {
     right: null,
   }));
 
-  // Construa a árvore de Huffman
   while (nodes.length > 1) {
-    // Ordene os nós com base em suas frequências (do menor para o maior)
     nodes.sort((a, b) => a.frequency - b.frequency);
 
-    // Crie um novo nó pai para os dois nós de menor frequência
     const leftChild = nodes.shift();
     const rightChild = nodes.shift();
     const parentNode = {
@@ -133,34 +131,25 @@ const buildHuffmanTree = (frequencies) => {
       right: rightChild,
     };
 
-    // Insira o novo nó na lista de nós
     nodes.push(parentNode);
   }
 
-  // O último nó restante é a raiz da árvore de Huffman
   return nodes[0];
 };
 
 const encodePixel = (huffmanTree, pixelValue) => {
-  // Percorra a árvore de Huffman até encontrar o nó correspondente ao pixel
   const traverseTree = (node, code) => {
-    // Caso base: se o nó for uma folha (pixel)
     if (!node.left && !node.right) {
-      // Retorne o código acumulado até o momento
       return code;
     }
 
-    // Se o pixel estiver à esquerda, adicione '0' ao código e continue percorrendo
     if (code.startsWith(0)) {
       return traverseTree(node.left, code.slice(1));
     }
 
-    // Se o pixel estiver à direita, adicione '1' ao código e continue percorrendo
     if (code.startsWith(1)) {
       return traverseTree(node.right, code.slice(1));
     }
   };
-
-  // Inicie a travessia da árvore de Huffman a partir da raiz
   return traverseTree(huffmanTree, pixelValue);
 };
